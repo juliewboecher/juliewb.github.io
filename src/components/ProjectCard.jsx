8853c38a10da1cbe50757be3002f0ee98e88fe9d
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-09-01 12:06:59 +0200

    ændret project section cards til row

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,15 @@
 function ProjectCard({ project }) {
   const { title, description, image } = project;
   return (
-    <div className="project-card">
-      <img className="project-image" src={`${image}`} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+    <article className="project-card">
+      <div classNme="project-card-left">
+        <img className="project-image" src={`${image}`} alt={title} />
+      </div>
+      <div className="project-card-right">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </article>
   );
 }
 

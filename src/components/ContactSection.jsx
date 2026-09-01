bbd22732021968a4547d72bbf6c6f281f7bc3fd7
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-09-01 13:23:21 +0200

    update contact information in ContactSection component

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,10 +4,18 @@ function ContactSection() {
   return (
     <section id="contact" className="contact-section">
       <h2>Kontakt</h2>
-      <p>mail: julie@example.com</p>
-      <p>telefon: +45 12345678</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/julieboecher/">linkedin.com/in/julieboecher/</a></p>
-      <p>GitHub: <a href="https://github.com/juliewb">github.com/juliewb</a></p>
+      <p>mail: julie@boecher.dk</p>
+      <p>telefon: +45 25124076</p>
+      <p>
+        LinkedIn:{" "}
+        <a href="https://www.linkedin.com/in/julie-wendelboe-bøcher-8954a4418">
+          linkedin.com/in/julieboecher/
+        </a>
+      </p>
+      <p>
+        GitHub:{" "}
+        <a href="https://github.com/juliewboecher">github.com/juliewboecher</a>
+      </p>
     </section>
   );
 }
